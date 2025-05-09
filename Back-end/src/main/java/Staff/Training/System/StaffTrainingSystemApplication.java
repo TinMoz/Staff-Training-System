21ed8945550587ddd22df40f480d51acb3232847
TinMoz <tinmozpoon2@gmail.com>
2025-05-09 23:22:03 +0800

    Test

--- Back-end/src/main/java/Staff/Training/System/StaffTrainingSystemApplication.java
+++ Back-end/src/main/java/Staff/Training/System/StaffTrainingSystemApplication.java
@@ -3,12 +3,24 @@ package staff.training.system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 // 主要應用程序，啟動Spring Boot應用程序
 @SpringBootApplication
 public class StaffTrainingSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StaffTrainingSystemApplication.class, args);
-	}
-
+		 Dotenv dotenv = Dotenv.configure()
+            .directory("src/main/resources")
+            .ignoreIfMissing()
+            .load();
+        dotenv.entries().forEach(entry -> {
+            if (System.getenv(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
+        SpringApplication.run(StaffTrainingSystemApplication.class, args);
+    }
 }
+
+
--- Back-end/src/main/java/Staff/Training/System/config/SecurityConfig.java
+++ Back-end/src/main/java/Staff/Training/System/config/SecurityConfig.java
@@ -59,7 +59,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() { 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); //前端地址
+        config.setAllowedOrigins(List.of("http://localhost:5173",
+        "staff-training-system-git-main-tinmozs-projects.vercel.app")); //前端地址
         config.setAllowedMethods(List.of("*")); //允許所有HTTP方法
         config.setAllowedHeaders(List.of("*")); //允許所有HTTP標頭
         config.setAllowCredentials(true); //允許攜帶憑證
